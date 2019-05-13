9cae7952de0a6ec7312bf376801522657e216b15
Tyler Brow <tymuska@yahoo.com>
2019-05-13 09:23:14 -0700

    some back-end and test map

--- Guardian-Web/client/src/components/App.js
+++ Guardian-Web/client/src/components/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import store from '../store'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+import Test from './actiontest'
+
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
+        <Router>
+          <Route path='/' component={Test} />
         <h1>Hello World</h1>
+        </Router>
       </Provider>
     )
   }
