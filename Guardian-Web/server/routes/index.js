90866715cda35a628b100379f3e134bd16820da1
Stephanie Revis <stephanie6304@gmail.com>
2019-05-15 11:38:10 -0700

    added login

--- Guardian-Web/server/routes/index.js
+++ Guardian-Web/server/routes/index.js
@@ -1,6 +1,64 @@
 var express = require('express');
 var router = express.Router();
-var pool=require('../db')
+var pool=require('../db');
+const sha512 = require("js-sha512");
+const jwt = require('jsonwebtoken');
+const config = require("config")
+
+
+router.post("/register", (req, res, next) => {
+ const username = req.body.username
+ const password = sha512(req.body.password + config.get("salt"))
+
+ const checksql = "SELECT count(1) as count from users WHERE username = ?"
+
+ conn.query(checksql, [username], (err, results, fields) => {
+   const count = results[0].count
+
+   if (count > 0) {
+     res.status(409).json({
+       error: "Username already taken"
+     })
+   } else {
+     const sql = "INSERT INTO users (username, password) VALUES (?, ?)"
+
+     conn.query(sql, [username, password], (err, results, fields) => {
+       if (err) {
+         throw new Error("register failed")
+       } else {
+         const token = jwt.sign({ username }, config.get("secret"))
+         res.json({
+           token: token
+         })
+       }
+     })
+   }
+ })
+})
+
+router.post("/login", (req, res, next) => {
+ const username = req.body.username
+ const password = sha512(req.body.password + config.get("salt"))
+
+ const sql =
+   "SELECT count(1) as count FROM users WHERE username = ? AND password = ?"
+
+ conn.query(sql, [username, password], (err, results, fields) => {
+   const count = results[0].count
+
+   if (count >= 1) {
+     const token = jwt.sign({ username }, config.get("secret"))
+
+     res.json({
+       token
+     })
+   } else {
+     res.status(401).json({
+       error: "Invalid username or password"
+     })
+   }
+ })
+})
 
 router.post('/notifications', (req, res, next) => {
   const sql = 'INSERT INTO notifications (message, date, day, time) VALUES (?, ?, ?, ?)'
@@ -58,3 +116,5 @@ module.exports = router;
 //        sendNotification()
 //} 
 // })}
+
+
